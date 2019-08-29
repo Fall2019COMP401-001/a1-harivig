2c2ec24835b4777de30f994ae1b81b2b2a1adeee
harivijay <harivijay@haris-mbp.wireless-1x.unc.edu>
2019-08-29 12:40:38 -0400

    test

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,7 +8,74 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
+		int numStoreItems = scan.nextInt();
+		String [] storeItemNames = new String [numStoreItems];
+		double [] storeItemPrices = new double [numStoreItems];
+		
 		// Your code follows here.
 		
+		for (int x = 0; x<numStoreItems;x++)
+		{
+			storeItemNames[x] = scan.next();
+			storeItemPrices[x] = scan.nextDouble();
+		}
+		
+		int numCust = scan.nextInt();
+		String [] custNames = new String [numCust];
+		double [] custTotals = new double [numCust];
+		
+		for (int y=0; y<numCust; y++)
+		{
+			custNames[y] = scan.next() + " " + scan.next();
+			
+			int numItemsSpent = scan.nextInt();
+			
+			for (int a=0;a<numItemsSpent;a++)
+			{
+				int numItemBought = scan.nextInt();
+				String itemBought = scan.next();
+				
+				for (int b=0; b<numStoreItems;b++)
+				{
+					if (itemBought.equals(storeItemNames[b]))
+					{
+						custTotals[y] += numItemBought*storeItemPrices[b];
+					}
+				}
+			}
+		}
+
+		double biggest = custTotals[0];
+		double smallest = custTotals[0];
+		int biggestIndex = 0;
+		int smallestIndex = 0;
+		
+		for (int test=0; test<numCust;test++)
+		{
+			if (custTotals[test]>biggest)
+			{
+				biggest = custTotals[test];
+				biggestIndex = test;
+			}
+			
+			if (custTotals[test]<smallest)
+			{
+				smallest = custTotals[test];
+				smallestIndex = test;		
+			}
+		}
+		
+		double totalTotalCost =0;
+		
+		for (int calc=0; calc<numCust; calc++)
+		{
+			totalTotalCost += custTotals[calc];
+		}
+		
+		double average = totalTotalCost/numCust;
+		
+		System.out.println("Biggest: "+ custNames[biggestIndex]+" ("+(String.format("%.2f",biggest))+")");
+		System.out.println("Smallest: "+ custNames[smallestIndex]+" ("+(String.format("%.2f",smallest))+")");
+		System.out.println("Average: "+(String.format("%.2f",average)));
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,10 +5,50 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
+				
 		Scanner scan = new Scanner(System.in);
-
+		
+		int tnum = scan.nextInt();
+		double totalCost = 0;
+		
 		// Your code follows here.
+		
+		String [] firstNames = new String [tnum];
+		String [] lastNames = new String [tnum];
+		double [] totalCostArray = new double [tnum];
+		char [] firstInitials = new char [tnum];
+		
+		for (int x=0; x<tnum; x++)
+		{
+			
+			firstNames[x] = scan.next();
+			firstInitials[x] = firstNames[x].charAt(0);
+			lastNames[x] = scan.next();
+			
+			int numItems = scan.nextInt();
+			
+			for (int y = 0; y<numItems; y++)
+			{
+				int quanItem = scan.nextInt();
+				
+				String nameItem = scan.next();
+				
+				double priceItem = scan.nextDouble();
+				
+				totalCost += quanItem*priceItem;
+			}
+		
+			totalCostArray[x] = totalCost;
+			totalCost = 0;
+			
+		} 
+		
+		for (int z=0; z<tnum;z++)
+		{
+			System.out.print(firstInitials[z]+". ");
+			System.out.print(lastNames[z]+": ");
+			System.out.println(String.format("%.2f",totalCostArray[z])); //used an online forum to figure out how to round to 2 decimal places
+		}
 		
 	}
 }
